57b238b89114829cd4f226066c56449b8c4017ef
Divyesh Shrestha <rashudshrestha12@gmail.com>
2022-11-10 16:03:27 +1100

    math operation added

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //Selectors
 const display1E1 = document.querySelector(".display-1")
 const display2E2 = document.querySelector(".display-2")
-const tempResultE1 = document.querySelector(".tem-result")
+const temResultE1 = document.querySelector(".tem-result")
 const numbersE1 = document.querySelectorAll(".number")
 const operationE1 = document.querySelectorAll(".operation")
 const equalE1 = document.querySelectorAll(".equal")
@@ -23,6 +23,62 @@ numbersE1.forEach((number)=>{
         }
         dis2Num += e.target.innerText
         display2E2.innerText= dis2Num
-
     })
+})
+operationE1.forEach((operation)=>{
+    operation.addEventListener("click", (e)=>{
+        if(!dis2Num)
+        return
+        haveDot=false
+        const operationName = e.target.innerText
+        if(dis1Num && dis2Num && lastOperation){//IF WE HAVE THREE TOGETHER THEN
+            mathOperation()
+        }else{
+            result = parseFloat(dis2Num)
+        }
+        clearVar(operationName)//clearVar moves the display to previoues display so we write the function below
+        lastOperation=operationName
+    })
+})
+function clearVar(name=""){
+    dis1Num += dis2Num + " "+ name + " "
+    display1E1.innerText=dis1Num;
+    display2E2.innerText=""
+    dis2Num=""
+    temResultE1.innerText=result
+}
+function mathOperation(){
+    if(lastOperation === "X"){
+        result = parseFloat(result) * parseFloat(dis2Num)//(result) is tem result
+    }else if(lastOperation === "+"){
+        result = parseFloat(result) + parseFloat(dis2Num)
+    }else if(lastOperation === "-"){
+        result = parseFloat(result) - parseFloat(dis2Num)
+    }else if(lastOperation === "/"){
+        result = parseFloat(result) / parseFloat(dis2Num)
+    }else if(lastOperation === "%"){
+        result = parseFloat(result) % parseFloat(dis2Num)
+    }
+}
+equalE1.addEventListener("click", (e)=>{
+    if(!dis1Num || !dis2Num) return
+    haveDot=false
+    mathOperation()
+    clearVar()
+    display2E2.innerText=result//it shows the result in main display as I donot want to show in tem result
+    temResultE1.innerText=""
+    dis2Num= result
+    dis1Num=""
+})
+clearAllE1.addEventListener("click", (e)=>{
+    display1E1.innerText=""
+    display2E2.innerText=""
+    dis1Num=""
+    dis2Num=""
+    result=""
+    temResultE1.innerText=""
+})
+clearLastE1.addEventListener("click",(e)=>{
+    display2E2.innerText=""
+    dis2Num=""
 })
